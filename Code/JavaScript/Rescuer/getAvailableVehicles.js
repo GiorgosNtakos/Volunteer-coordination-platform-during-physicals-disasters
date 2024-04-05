00756d23bd27996acd77b50ee939013c402e6944
Giorgos Ntakos <giorgosdakos23456@gmail.com>
2024-04-05 15:35:37 +0300

    Λειτουργιες για την επιλογη διαθέσιμων οχημάτων αλλα και του φορτίου στο οχημα

---
+++ Code/JavaScript/Rescuer/getAvailableVehicles.js
@@ -0,0 +1,141 @@
+document.addEventListener("DOMContentLoaded", function () {
+    getAvailableVehicles()
+
+});
+
+function getAvailableVehicles(){
+    var vehiclesList = document.getElementById("vehicle_list");
+
+    $.ajax({
+        url: "http://localhost/webproject/Code/PHP/Rescuer/get_available_vehicles.php",
+        method: "GET",
+        success: function (response) {
+          if (response.status === "success") {
+            console.log(response);
+            // Τώρα έχετε τα δεδομένα των προϊόντων από τον server
+    
+            var vehicles = response.vehicles;
+    
+            vehiclesList.innerHTML = "";
+    
+            // Εξετάζουμε τα δεδομένα των προϊόντων και δημιουργούμε τα table rows
+            vehicles.forEach(function (vehicle) {
+              var row = document.createElement("tr");
+    
+              // Ονομασία προϊόντος
+              var nameCell = document.createElement("td");
+              var decorateIcon = document.createElement("span");
+              decorateIcon.innerHTML = "<i id = 'decorate' class='fa'>&#xf0d1;</i>";
+              nameCell.textContent = vehicle.name;
+              nameCell.appendChild(decorateIcon);
+              row.appendChild(nameCell);
+    
+              // Κατηγορία
+              var crewCell = document.createElement("td");
+              crewCell.textContent = vehicle.crew;
+              row.appendChild(crewCell);
+    
+              // Ποσότητα
+              var streetCell = document.createElement("td");
+              streetCell.textContent = vehicle.street;
+              row.appendChild(streetCell);
+    
+              // λεπτομέρειες
+              var numberCell = document.createElement("td");
+              numberCell.textContent = vehicle.number;
+              row.appendChild(numberCell);
+    
+              // Ημερομηνια Εισαγωγης
+              var townCell = document.createElement("td");
+              townCell.textContent = vehicle.town;
+              row.appendChild(townCell);
+    
+              var ChoiseButtonCell = document.createElement("td");
+              var assignButton = document.createElement("span");
+              assignButton.title = "Επιλογή Οχήματος";
+              assignButton.innerHTML = "<i id='choose'class='fa'>&#xf055;</i>";
+              assignButton.style.marginRight = "20px";
+              assignButton.addEventListener("click", function () {
+                // Καλείτε τη συνάρτηση για διαγραφή του προϊόντος με το vehicle.name
+                assignvehicle(vehicle.id);
+              });
+              ChoiseButtonCell.appendChild(assignButton);
+    
+              row.appendChild(ChoiseButtonCell);
+    
+              // Προσθέστε τη σειρά στον πίνακα
+              vehiclesList.appendChild(row);
+            });
+          } else if (response.status === "success_but_empty") {
+            vehiclesList.innerHTML =
+              "<tr><td colspan='7'>Δεν υπάρχουν διαθέσιμα Οχήματα.</td></tr>";
+            showMessage("success-message", response.message, "#vehicles_list");
+          } else {
+            vehiclesList.innerHTML =
+              "<tr><td colspan='7'>Προέκυψε σφάλμα κατά τη φόρτωση των διαθέσιμων οχημάτων.</td></tr>";
+            showMessage(
+              "error-message",
+              "Ενώ η ανάκτηση των διαθέσιμων οχημάτων ήταν επιτυχής κάτι πήγε στραβά. Παρακαλώ δοκιμάστε ξανά.",
+              "#tables_title"
+            );
+          }
+        },
+        error: function (response) {
+          var errorResponse = JSON.parse(response.responseText);
+    
+          if (errorResponse.status === "wrong_method_405") {
+            showMessage("error-message", errorResponse.message, "#tables_title");
+          } else if (errorResponse.status === "server_error") {
+            showMessage("error-message", errorResponse.message, "#tables_title");
+          } else {
+            showMessage(
+              "error-message",
+              "Σφάλμα κατά τη διάρκεια φόρτωσης των διαθέσιμων οχημάτων. Παρακαλώ δοκιμάστε ξανά.",
+              "delete-all-button"
+            );
+          }
+        },
+      });
+}
+
+function assignvehicle(id){
+  $.ajax({
+    url: "http://localhost/webproject/Code/PHP/Rescuer/assign_to_vehicle.php",
+    method: "POST",
+    data: {vehicle_id:id},
+    success: function (response) {
+      if (response.status === "success") {
+        showMessage("success-message", response.message, "#tables_title");
+      } else {
+        showMessage(
+          "error-message",
+          "Ενώ η ανάθεση ήταν επιτυχής κάτι πήγε στραβά. Παρακαλώ δοκιμάστε ξανα.",
+          "#tables_title"
+        );
+      }
+    },
+    error: function (response) {
+      var errorResponse = JSON.parse(response.responseText);
+      if (errorResponse.status === "need_connection") {
+        window.location.href = "login_signup.html";
+        showMessage("error-message", errorResponse.message, "#tables_title");
+      } else if (errorResponse.status === "wrong_method_405") {
+        showMessage("error-message", errorResponse.message, "#tables_title");
+      } else if (errorResponse.status === "server_error") {
+        showMessage("error-message", errorResponse.message, "#tables_title");
+      } else if (errorResponse.status === "missing_400") {
+        showMessage("error-message", errorResponse.message, "#tables_title");
+      } else if(errorResponse.status === "already_assigned"){
+        showMessage("error-message", errorResponse.message, "#tables_title");
+      }else {
+        showMessage(
+          "error-message",
+          "Σφάλμα κατά την διάρκεια ανάθεση σας στο όχημα. Παρακαλώ δοκιμάστε ξανά.",
+          "#deleteCategory"
+        );
+      }
+    },
+
+  });
+
+}
---
+++ Code/JavaScript/Rescuer/vehicle_items_operations.js
@@ -0,0 +1,446 @@
+//! NA FTIAKSO SWSTA RA RESETS STO CARGO PAGE ME TA FILTRA
+
+document.addEventListener("DOMContentLoaded", function () {
+  var numberPattern = /^[1-9]\d*$/;
+  const unloadButton = document.getElementById("unload-vehicle-cargo");
+  const AddItemInCargo = document.getElementById("add-stock-form");
+  const removeStockCargo = document.getElementById("remove-stock-form");
+
+  unloadButton.addEventListener("click", function () {
+    unloadAllCargo();
+  });
+
+  AddItemInCargo.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var itemQuantity = document.getElementById("a_quantity").value;
+
+    if (itemQuantity.trim() === "") {
+      showMessage(
+        "error-message",
+        "Παρακαλώ εισάγετε την ποσότητα του είδους.",
+        "#a_quantity"
+      );
+      return;
+    }
+
+    if (!numberPattern.test(itemQuantity)) {
+      showMessage(
+        "error-message",
+        "Η ποσότητα πρέπει να είναι ακέραια και οχι αρνητική.",
+        "#a_quantity"
+      );
+      return;
+    }
+
+    addItemInCargo(itemQuantity);
+  });
+
+  removeStockCargo.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var removeQuantity = document.getElementById("r_quantity").value;
+
+    if (removeQuantity.trim() === "") {
+      showMessage(
+        "error-message",
+        "Παρακαλώ εισάγετε την ποσότητα του είδους.",
+        "#r_quantity"
+      );
+      return;
+    }
+
+    if (!numberPattern.test(removeQuantity)) {
+      showMessage(
+        "error-message",
+        "Η ποσότητα πρέπει να είναι ακέραια και οχι αρνητική.",
+        "#r_quantity"
+      );
+      return;
+    }
+
+    removeStockItem(removeQuantity);
+    updateItemsBasedOnCategoryAndSearch();
+  });
+
+});
+
+function getitemInformation(itemName) {
+    $.ajax({
+      url: "http://localhost/webproject/Code/PHP/Admin/get_item_info.php",
+      method: "GET",
+      data: { itemName: itemName },
+      success: function (response) {
+        if (response.status === "success") {
+          document.getElementById("a_name").readOnly = false;
+          document.getElementById("a_name").value = response.item_info.name;
+          document.getElementById("a_quantity").value = response.item_info.quantity;
+          document.getElementById("a_name").readOnly = true;
+          showMessage("success-message", response.message, "#add-stock-form");
+        } else if (response.status === "success_but_empty") {
+          showMessage("success-message", response.message, "#add-stock-form");
+        } else {
+          showMessage(
+            "error-message",
+            "Ενώ η ανάκτηση πληροφοριών του είδους ήταν επιτυχής κάτι πήγε στραβά. Παρακαλώ δοκιμάστε ξανα.",
+            "#add-stock-form"
+          );
+        }
+      },
+      error: function (response) {
+        var errorResponse = JSON.parse(response.responseText);
+  
+        if (errorResponse.status === "wrong_method_405") {
+          showMessage(
+            "error-message",
+            errorResponse.message,
+            "#add-stock-form"
+          );
+        } else if (errorResponse.status === "server_error") {
+          showMessage(
+            "error-message",
+            errorResponse.message,
+            "#add-stock-form"
+          );
+        } else if (errorResponse.status === "missing_400") {
+          showMessage(
+            "error-message",
+            errorResponse.message,
+            "#add-stock-form"
+          );
+        } else {
+          showMessage(
+            "error-message",
+            "Προέκυψε σφάλμα κατά την ανάκτηση πληροφοριών του είδους. Παρακαλώ δοκιμάστε ξανά.",
+            "#items-list"
+          );
+        }
+      },
+    });
+  }
+
+  function getVehicleitemInformation(itemName) {
+    $.ajax({
+      url: "http://localhost/webproject/Code/PHP/Rescuer/get_vehicle_item_info.php",
+      method: "GET",
+      data: { itemName: itemName },
+      success: function (response) {
+        if (response.status === "success") {
+          document.getElementById("r_name").readOnly = false;
+          document.getElementById("r_name").value = response.item_info.name;
+          document.getElementById("r_quantity").value = response.item_info.quantity;
+          document.getElementById("r_name").readOnly = true;
+          showMessage("success-message", response.message, "#remove-stock-form");
+        } else if (response.status === "success_but_empty") {
+          showMessage("success-message", response.message, "#remove-stock-form");
+        } else {
+          showMessage(
+            "error-message",
+            "Ενώ η ανάκτηση πληροφοριών του είδους ήταν επιτυχής κάτι πήγε στραβά. Παρακαλώ δοκιμάστε ξανα.",
+            "#remove-stock-form"
+          );
+        }
+      },
+      error: function (response) {
+        var errorResponse = JSON.parse(response.responseText);
+  
+        if (errorResponse.status === "wrong_method_405") {
+          showMessage(
+            "error-message",
+            errorResponse.message,
+            "#remove-stock-form"
+          );
+        } else if (errorResponse.status === "server_error") {
+          showMessage(
+            "error-message",
+            errorResponse.message,
+            "#remove-stock-form"
+          );
+        } else if (errorResponse.status === "vehicle_not_found") {
+            showMessage(
+              "error-message",
+              errorResponse.message,
+              "#remove-stock-form"
+            );
+          } else if (errorResponse.status === "need_connection") {
+            showMessage(
+              "error-message",
+              errorResponse.message,
+              "#remove-stock-form"
+            );
+          }  else if (errorResponse.status === "missing_400") {
+          showMessage(
+            "error-message",
+            errorResponse.message,
+            "#remove-stock-form"
+          );
+        } else {
+          showMessage(
+            "error-message",
+            "Προέκυψε σφάλμα κατά την ανάκτηση πληροφοριών του είδους. Παρακαλώ δοκιμάστε ξανά.",
+            "#items-list"
+          );
+        }
+      },
+    });
+  }
+
+  function isInDistance(){
+    return new Promise((resolve, reject) => {
+    $.ajax({
+        url: "http://localhost/webproject/Code/PHP/Rescuer/distance_VW.php",
+        method: "GET",
+        success: function (response) {
+          if (response.status === "success") {
+            resolve(response.distance <= 100);
+          } else if (response.status === "too_far"){
+            resolve(false);
+          }
+        },
+        error: function (response) {
+          var errorResponse = JSON.parse(response.responseText);
+          reject(new Error(errorResponse.message));
+
+        },
+      });
+    });
+    }
+
+    function unloadAllCargo(){
+      isInDistance().then(isClose => {
+        if(isClose) {
+      $.ajax({
+        url: "http://localhost/webproject/Code/PHP/Rescuer/unload_all_cargo.php",
+        method: "DELETE",
+        success: function (response) {
+          if (response.status === "success") {
+            showMessage(
+              "success-message",
+              response.message,
+              "#items-list"
+            );
+          } else if(response.status === "cargo_empty"){
+            showMessage(
+              "success-message",
+              response.message,
+              "#items-list"
+            );
+          } else{
+            showMessage(
+              "error-message",
+              "Ενώ ήταν επιτυχής η διαδικασία προέκυψε κάκοιο σφάλμα. Παρακαλώ δοκιμάστε ξανά.",
+              "#items-list"
+            );
+          }
+        },
+        error: function (response) {
+          var errorResponse = JSON.parse(response.responseText);
+
+          if (errorResponse.status === "wrong_method_405") {
+            showMessage(
+              "error-message",
+              errorResponse.message,
+              "#items-list"
+            );
+          } else if (errorResponse.status === "server_error") {
+            showMessage(
+              "error-message",
+              errorResponse.message,
+              "#items-list"
+            );
+          } else if (errorResponse.status === "vehicle_not_found") {
+              showMessage(
+                "error-message",
+                errorResponse.message,
+                "#items-list"
+              );
+            } else if (errorResponse.status === "need_connection") {
+              showMessage(
+                "error-message",
+                errorResponse.message,
+                "#items-list"
+              );
+            }  else {
+            showMessage(
+              "error-message",
+              "Προέκυψε σφάλμα κατά την διαδικασία. Παρακαλώ δοκιμάστε ξανά.",
+              "#items-list"
+            );
+          }
+        },
+      });
+    } else{
+      showMessage(
+        "error-message",
+        "Το όχημα δεν βρίσκεται σε ακτίνα 100 μέτρων από την Βάση/Αποθήκη.",
+        "#items-list"
+    );
+   }
+ }).catch(error =>{
+  console.error("Σφάλμα κατά τον έλεγχο της απόστασης", error);
+ });
+}
+
+function addItemInCargo(quantity){
+  isInDistance().then(isClose => {
+    if(isClose) {
+  $.ajax({
+    url: "http://localhost/webproject/Code/PHP/Rescuer/add_items_quantity.php",
+    method: "POST",
+    data:{quantity : quantity},
+    success: function (response) {
+      if (response.status === "success") {
+        showMessage(
+          "success-message",
+          response.message,
+          "#add-stock-form"
+        );
+      } else{
+        showMessage(
+          "error-message",
+          "Ενώ ήταν επιτυχής η διαδικασία προέκυψε κάκοιο σφάλμα. Παρακαλώ δοκιμάστε ξανά.",
+          "#add-stock-form"
+        );
+      }
+    },
+    error: function (response) {
+      var errorResponse = JSON.parse(response.responseText);
+
+      if (errorResponse.status === "wrong_method_405") {
+        showMessage(
+          "error-message",
+          errorResponse.message,
+          "#add-stock-form"
+        );
+      } else if (errorResponse.status === "server_error") {
+        showMessage(
+          "error-message",
+          errorResponse.message,
+          "#add-stock-form"
+        );
+      } else if (errorResponse.status === "vehicle_not_found") {
+          showMessage(
+            "error-message",
+            errorResponse.message,
+            "#add-stock-form"
+          );
+        } else if (errorResponse.status === "noSuch_quantity") {
+          showMessage(
+            "error-message",
+            errorResponse.message,
+            "#add-stock-form"
+          );
+        }  else if (errorResponse.status === "missing_400") {
+          showMessage(
+            "error-message",
+            errorResponse.message,
+            "#add-stock-form"
+          );
+        } else if (errorResponse.status === "need_connection") {
+          showMessage(
+            "error-message",
+            errorResponse.message,
+            "#add-stock-form"
+          );
+        }  else {
+        showMessage(
+          "error-message",
+          "Προέκυψε σφάλμα κατά την διαδικασία. Παρακαλώ δοκιμάστε ξανά.",
+          "#add-stock-form"
+        );
+      }
+    },
+  });
+} else{
+  showMessage(
+    "error-message",
+    "Το όχημα δεν βρίσκεται σε ακτίνα 100 μέτρων από την Βάση/Αποθήκη.",
+    "#items-list"
+);
+}
+}).catch(error =>{
+console.error("Σφάλμα κατά τον έλεγχο της απόστασης", error);
+});
+}
+
+function removeStockItem(quantity){
+  isInDistance().then(isClose => {
+    if(isClose) {
+  $.ajax({
+    url: "http://localhost/webproject/Code/PHP/Rescuer/remove_stock_items.php",
+    method: "POST",
+    data:{quantity : quantity},
+    success: function (response) {
+      if (response.status === "success") {
+        showMessage(
+          "success-message",
+          response.message,
+          "#remove-stock-form"
+        );
+      } else{
+        showMessage(
+          "error-message",
+          "Ενώ ήταν επιτυχής η διαδικασία προέκυψε κάκοιο σφάλμα. Παρακαλώ δοκιμάστε ξανά.",
+          "#remove-stock-form"
+        );
+      }
+    },
+    error: function (response) {
+      var errorResponse = JSON.parse(response.responseText);
+
+      if (errorResponse.status === "wrong_method_405") {
+        showMessage(
+          "error-message",
+          errorResponse.message,
+          "#remove-stock-form"
+        );
+      } else if (errorResponse.status === "server_error") {
+        showMessage(
+          "error-message",
+          errorResponse.message,
+          "#remove-stock-form"
+        );
+      } else if (errorResponse.status === "vehicle_not_found") {
+          showMessage(
+            "error-message",
+            errorResponse.message,
+            "#remove-stock-form"
+          );
+        } else if (errorResponse.status === "noSuch_quantity") {
+          showMessage(
+            "error-message",
+            errorResponse.message,
+            "#remove-stock-form"
+          );
+        } else if (errorResponse.status === "need_connection") {
+          showMessage(
+            "error-message",
+            errorResponse.message,
+            "#remove-stock-form"
+          );
+        } else if (errorResponse.status === "missing_400") {
+          showMessage(
+            "error-message",
+            errorResponse.message,
+            "#remove-stock-form"
+          );
+        } else {
+        showMessage(
+          "error-message",
+          "Προέκυψε σφάλμα κατά την διαδικασία. Παρακαλώ δοκιμάστε ξανά.",
+          "#remove-stock-form"
+        );
+      }
+    },
+  });
+} else{
+  showMessage(
+    "error-message",
+    "Το όχημα δεν βρίσκεται σε ακτίνα 100 μέτρων από την Βάση/Αποθήκη.",
+    "#items-list"
+);
+}
+}).catch(error =>{
+console.error("Σφάλμα κατά τον έλεγχο της απόστασης", error);
+});
+}
