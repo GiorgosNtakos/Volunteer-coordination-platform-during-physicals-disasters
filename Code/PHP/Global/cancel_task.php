8b61300baebff32f9ed17a2d5682dc70b3b71b7c
jmetaj <jmetaj400@gmail.com>
2024-06-17 16:09:26 +0300

    cancel_task

--- Code/PHP/Global/cancel_task.php
+++ Code/PHP/Global/cancel_task.php
@@ -10,21 +10,24 @@ $conn->set_charset("utf8");
 $response = array();
 
 if (isset($_SESSION['user_auth'])) {
-    $username = $_SESSION['user_auth']['username'];
+    $user_id = $_SESSION['user_auth']['id'];
 
     if (isset($_POST['task_id'])) {
         $task_id = $_POST['task_id'];
 
         // Check if the task exists and is assigned to the rescuer
-        $sql = "SELECT id FROM Tasks WHERE id = ? AND status = 'accepted' AND user_id = (SELECT id FROM Users WHERE username = ?)";
+        $sql = "SELECT Tasks.id 
+                FROM Tasks 
+                INNER JOIN VehicleAssignments ON VehicleAssignments.vehicle_id = Tasks.vehicle_id 
+                WHERE Tasks.id = ? AND Tasks.status = 'accepted' AND VehicleAssignments.user_id = ?";
         $stmt = $conn->prepare($sql);
-        $stmt->bind_param('ss', $task_id, $username);
+        $stmt->bind_param('ss', $task_id, $user_id);
         $stmt->execute();
         $result = $stmt->get_result();
 
         if ($result->num_rows > 0) {
             // Update the task status to 'pending' and remove the user_id
-            $sql = "UPDATE Tasks SET status = 'pending', user_id = NULL, updated_at = NOW() WHERE id = ?";
+            $sql = "UPDATE Tasks SET status = 'pending', vehicle_id = NULL, updated_at = NOW() WHERE id = ?";
             $stmt = $conn->prepare($sql);
             $stmt->bind_param('s', $task_id);
 
